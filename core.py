925f2c391c083bc292d694f9503a827eb50ad0b9
MrKiven <kiven.mr@gmail.com>
2015-03-02 13:56:39 +0800

    modify the core

--- core.py
+++ core.py
@@ -5,9 +5,7 @@ __author__ = 'kiven'
 class Brain(object):
     """the core of robot"""
     def __init__(self, arg):
-        super(ClassName, self).__init__()
         self.arg = arg
 
     def _thinking(self,*args,**kwarg):
-        pass
 
