0ff9b37688dbbb387dd763802f0d51f9ad84aeb0
MrKiven <kiven.mr@gmail.com>
2015-03-02 13:55:16 +0800

    core of robot

---
+++ core.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'kiven'
+
+class Brain(object):
+    """the core of robot"""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+
+    def _thinking(self,*args,**kwarg):
+        pass
+
